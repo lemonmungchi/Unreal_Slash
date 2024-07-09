f9ca187b8febbab652a84e367e9df36c862cb3a4
Hyeon Chan <116485176+lemonmungchi@users.noreply.github.com>
2024-07-09 23:43:05 +0900

    7.9

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -14,6 +14,11 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 	
 	UE_LOG(LogTemp,Warning,TEXT("Begin Play called!"));
+
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen!!!!"));
+	}
+
 }
 
 // Called every frame
@@ -21,5 +26,13 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("Delta Time: %f"), DeltaTime);
+
+	if (GEngine) {
+
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Delta Time: %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
+	}
 }
 
