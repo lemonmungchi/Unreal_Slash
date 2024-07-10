eb194f918d58a750bf4775ae8a080934fbc1aa6e
Hyeon Chan <116485176+lemonmungchi@users.noreply.github.com>
2024-07-10 23:25:49 +0900

    7.10 디버깅

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -1,4 +1,7 @@
 #include "Items/Item.h"
+#include "Slash/DebugMacros.h"
+
+
 
 // Sets default values
 AItem::AItem()
@@ -12,12 +15,16 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	UE_LOG(LogTemp,Warning,TEXT("Begin Play called!"));
 
-	if (GEngine) {
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen!!!!"));
-	}
+	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+
+	DRAW_SPHERE(Location);
+	/*DRAW_LINE(Location, Location + Forward * 100.f);
+	DRAW_POINT(Location + Forward * 100.f);*/
+
+	DRAW_VECTOR(Location, Location + Forward * 100.f);
 
 }
 
@@ -26,13 +33,5 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("Delta Time: %f"), DeltaTime);
-
-	if (GEngine) {
-
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Delta Time: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-	}
 }
 
